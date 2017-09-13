91970e52eb803fb5d664c9b3017dc589ef3672dc
Molly Nicholas <molecule@berkeley.edu>
2017-09-13 10:01:52 -0700

    clean up

--- app/src/main/java/lab/ecologies/hybrid/alterwear/MainActivity.java
+++ app/src/main/java/lab/ecologies/hybrid/alterwear/MainActivity.java
@@ -14,14 +14,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nxp.nfclib.CardType;
 import com.nxp.nfclib.NxpNfcLib;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,11 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
 
-    private Tag tag = null;
     private Activity main;
-    private TextView textView = null;
     private TextView record = null;
-    private EditText editText_message = null;
     private String strKey = "3969956a568c92252638524453df1091";
     private NxpNfcLib libInstance = null; // TapLinx Library
     private NdefMessage message = null;
@@ -53,30 +47,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         main = this;
 
-        textView = (TextView) findViewById(R.id.mainTextView);
         record = (TextView) findViewById(R.id.record);
-        editText_message = (EditText) findViewById(R.id.editText_message);
         v = findViewById(R.id.demoLayout);
         initializeLibrary();
-
-        Button btn_send = (Button) findViewById(R.id.btn_send);
-        btn_send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Log.d(TAG, "Message from Edit Text: " + editText_message.getText().toString());
-                    message = createNdefTextMessage(editText_message.getText().toString());
-                    if (message != null) {
-                        dialog = new ProgressDialog(MainActivity.this);
-                        dialog.setMessage("Tag NFC Tag please");
-                        dialog.show();
-                    }
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
 
         Button btn_send_zero = (Button) findViewById(R.id.btn_send_zero);
         btn_send_zero.setOnClickListener(new View.OnClickListener() {
@@ -208,73 +181,9 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        /*
-        if (message != null) {
-            nfcMger.writeTag(currentTag, message);
-            dialog.dismiss();
-            Toast.makeText(getApplicationContext(), "Tag written", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            // Handle intent
-
-        }
-
-        */
         super.onNewIntent( intent );
     }
 
-    private void doNdef() throws IOException {
-        // NDEF Message to write in the tag
-        NdefMessage msg = null;
-        msg = createNdefTextMessage(editText_message.getText().toString());
-        try {
-            // Time statistics to return
-            long timeNdefWrite = 0;
-            long RegTimeOutStart = System.currentTimeMillis();
-
-            // Write the NDEF using NfcA commands to avoid problems when dealing with protected tags
-            // Calling reader close / connect resets the authenticated status
-            Log.d(TAG, "doNdef, about to writeNDEF w/ this msg:" + msg.describeContents());
-            reader.writeNDEF(msg, null);
-
-            timeNdefWrite = System.currentTimeMillis() - RegTimeOutStart;
-            dialog.dismiss();
-            Toast.makeText(main, "time to write: " + timeNdefWrite, Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Toast.makeText(main, "write tag failed", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
-    }
-
-    // https://www.mifare.net/wp-content/uploads/2016/08/AN11876-Starting-Development-with-TapLinx-SDK.pdf
-    private void cardLogic (final Intent intent) {
-        CardType cardType = libInstance.getCardType( intent );
-        Log.d( TAG, "Card type found: " + cardType.getTagName());
-        textView.setText("Card type found: " + cardType.getTagName());
-
-
-        if (CardType.NTagI2CPlus1K== cardType) {
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
-            //IMFClassicEV1 objClassic = ClassicFactory.getInstance().getClassicEV1( MifareClassic.get( tag));
-
-            /*
-            INTAGI2Cplus nTagI2C = NTagFactory.getInstance().getNTAGI2CPlus1K(Ndef.get(tag));
-            if ( ! nTagI2C.getReader().isConnected()) {
-                nTagI2C.getReader().connect();
-            }
-            NdefMessage msg = null;
-            try {
-                msg = createNdefTextMessage(message.toString());
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-    */
-            //nTagI2C.writeNDEF(msg);
-        }
-
-    }
 
     private NdefMessage createNdefTextMessage(String text) throws UnsupportedEncodingException {
         Log.d(TAG, "crateNdefTextMessage, text: " + text);
