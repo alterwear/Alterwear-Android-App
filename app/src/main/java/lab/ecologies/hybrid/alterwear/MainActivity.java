1051e4076760385c8ce2a5380bf41d16bef7d66a
Molly Nicholas <molecule@berkeley.edu>
2018-10-23 08:10:49 -0700

    remove extraneous bit info from ndef message

--- app/src/main/java/lab/ecologies/hybrid/alterwear/MainActivity.java
+++ app/src/main/java/lab/ecologies/hybrid/alterwear/MainActivity.java
@@ -153,7 +153,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 try {
                     Log.d(TAG, "Send a chunk of a bitmap");
-                    message = createNdefTextMessage("0x01");
+                    //message = createNdefTextMessage("0xFF, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0xFF");
+                    message = createNdefTextMessage("FF  FF 00 FF 00 FF FF");
                     if (message != null) {
                         dialog = new ProgressDialog(MainActivity.this);
                         dialog.setMessage("Tag NFC Tag please");
